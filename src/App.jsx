a55d12c284abb9d5bb40716a46ee7e25f0c096f2
Ayush H <jqghebri@gmail.com>
2024-08-17 02:12:02 +0530

    creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [length, setLength] = useState(16);
-  const [numberAllowed, setNumberAllowed] = useState(true);
-  const [characterAllowed, setCharacterAllowed] = useState(true);
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
   //useRef hook
   const passwordRef = useRef(null);
 
-  const passwordGeneraor = useCallback(() => {
+  const passwordGenerator = useCallback(() => {
     let pass = "";
-    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    if (numberAllowed) {
-      str = +"0123456789";
-    }
-    if (characterAllowed) {
-      str += "!@#$%^&*()_+-=[]{}|~";
-    }
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
 
     setPassword(pass);
-  }, [length, numberAllowed, characterAllowed, setPassword]);
-
-
-
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
@@ -42,18 +34,22 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
 
-
-
+  
   return (
+
+
+
+
     <div className="w-full max-w-xl mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <h1 className="text-white text-center my-3">Password generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
           type="text"
           value={password}
-          className="outline-none w-full py-2 px-6 my-5 rounded-lg"
-          placeholder="password"
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
           readOnly
+          ref={passwordRef}
         />
         <button
           onClick={copyPasswordToClipboard}
@@ -90,7 +86,7 @@ function App() {
         <div className="flex items-center gap-x-1">
           <input
             type="checkbox"
-            defaultChecked={characterAllowed}
+            defaultChecked={charAllowed}
             id="characterInput"
             onChange={() => {
               setCharAllowed((prev) => !prev);
