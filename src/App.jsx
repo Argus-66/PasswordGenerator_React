08b6240de036a975f8247a5d2c039b9fa5ddc5b6
Ayush H <jqghebri@gmail.com>
2024-08-17 01:35:19 +0530

    creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -6,15 +6,39 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(true)
   const [characterAllowed, setCharacterAllowed] = useState(true)
   const [uppercaseAllowed, setUppercaseAllowed] = useState(true)
-  const [specialCharacterAllowed, setSpecialCharacterAllowed] = useState(true)
   const [password, setPassword] = useState('')
 
 
-  const [passwordGeneraor] = () => {}
+  const [passwordGeneraor] = useCallback(()=> {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyz"
+
+    if (numberAllowed) {
+      str =+ "0123456789"
+    }
+    if (characterAllowed) {
+      str += "!@#$%^&*()_+-=[]{}|~"
+    }
+    if (uppercaseAllowed) {
+      str =+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, characterAllowed, , uppercaseAllowed, setPassword]) 
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-autop shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+      test
+      </div>
     </>
   )
 }
