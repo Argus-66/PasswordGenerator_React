629a17fe48825977a985ece25c410263f44b50be
Ayush H <jqghebri@gmail.com>
2024-08-17 02:12:33 +0530

    creation

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const passwordRef = useRef(null);
 
 
-  
+
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -49,6 +49,7 @@ function App() {
     <div className="w-full max-w-xl mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className="text-white text-center my-3">Password generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
         <input
           type="text"
           value={password}
@@ -57,12 +58,14 @@ function App() {
           readOnly
           ref={passwordRef}
         />
+
         <button
           onClick={copyPasswordToClipboard}
           className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
         >
           copy
         </button>
+        
       </div>
       <div className="flex text-sm gap-x-2">
         <div className="flex items-center gap-x-1">
